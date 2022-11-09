946a088b3167272fda7e01d8f8c60e5cd0d64de9
James Berger <62909049+jamesberger55@users.noreply.github.com>
2022-11-09 13:30:31 -0600

    Update database-schema.sql

--- webapp/docs/database-schema.sql
+++ webapp/docs/database-schema.sql
@@ -1,26 +1,29 @@
 CREATE TABLE contestants (
         contestant_name TEXT,
-        season_number INTEGER,
         age INTEGER,
         hometown TEXT,
         occupation TEXT,
+        season_number INTEGER,
+        finish INTEGER,
+        gender TEXT,
         african_american BIT,
-        latin_american BIT,
         asian_american BIT,
+        latin_american BIT
+        
     );
 
 CREATE TABLE season (
-        season_name TEXT,
         season_number INTEGER,
-        num_contestant INTEGER,
+        season_name TEXT,
+        num_contestant INTEGER
     );
 
 CREATE TABLE tribe (
         season_number INTEGER,
         tribe_name TEXT,
         size INTEGER,
-        poc INTEGER,
-        merged BIT
+        merged INTEGER,
+        poc INTEGER
 
     );
 
