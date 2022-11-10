aadef8b90dbeaf65402ab3704d4c7660c0941c0c
Alex Widman <alexwidman@Alexs-MacBook-Pro-3.local>
2022-11-09 19:28:41 -0600

    adding .js

--- webapp/static/webapp.js
+++ webapp/static/webapp.js
@@ -1,15 +1,16 @@
 /*
- * books.js
- * Jeff Ondich, 27 April 2016
- * Updated, 5 November 2020
+ * webapp.js
+ * James Berger, Aldo Polanco, Alex Widman
+ * Nov 9th, 2022
+ * Updated, Nov 9th, 2022
  */
 
 window.onload = initialize;
 
 function initialize() {
-    loadAuthorsSelector();
+    loadContestantsSelector();
 
-    let element = document.getElementById('author_selector');
+    let element = document.getElementById('contestant_selector');
     if (element) {
         element.onchange = onAuthorsSelectionChanged;
     }
@@ -25,29 +26,29 @@ function getAPIBaseURL() {
     return baseURL;
 }
 
-function loadAuthorsSelector() {
-    let url = getAPIBaseURL() + '/authors/';
+function loadContestantsSelector() {
+    let url = getAPIBaseURL() + '/contestants/';
 
     // Send the request to the books API /authors/ endpoint
     fetch(url, {method: 'get'})
 
     // When the results come back, transform them from a JSON string into
-    // a Javascript object (in this case, a list of author dictionaries).
+    // a Javascript object (in this case, a list of contestant dictionaries).
     .then((response) => response.json())
 
     // Once you have your list of author dictionaries, use it to build
-    // an HTML table displaying the author names and lifespan.
-    .then(function(authors) {
+    // an HTML table displaying the contestant name, age, and season.
+    .then(function(contestants) {
         // Add the <option> elements to the <select> element
         let selectorBody = '';
-        for (let k = 0; k < authors.length; k++) {
-            let author = authors[k];
-            selectorBody += '<option value="' + author['id'] + '">'
-                                + author['surname'] + ', ' + author['given_name']
+        for (let k = 0; k < contestants.length; k++) {
+            let contestant = contestants[k];
+            selectorBody += '<option value="' + contestant['id'] + '">'
+                                + contestant['contestant_name'] + ', ' + contestant['age'] + ', ' + contestant['season_number']
                                 + '</option>\n';
         }
 
-        let selector = document.getElementById('author_selector');
+        let selector = document.getElementById('contestant_selector');
         if (selector) {
             selector.innerHTML = selectorBody;
         }
@@ -58,39 +59,3 @@ function loadAuthorsSelector() {
         console.log(error);
     });
 }
-
-function onAuthorsSelectionChanged() {
-    let element = document.getElementById('author_selector');
-    if (!element) {
-        return;
-    }
-    let authorID = element.value; 
-
-    let url = getAPIBaseURL() + '/books/author/' + authorID;
-
-    fetch(url, {method: 'get'})
-
-    .then((response) => response.json())
-
-    .then(function(books) {
-        let tableBody = '';
-        for (let k = 0; k < books.length; k++) {
-            let book = books[k];
-            tableBody += '<tr>'
-                            + '<td>' + book['title'] + '</td>'
-                            + '<td>' + book['publication_year'] + '</td>'
-                            + '</tr>\n';
-        }
-
-        // Put the table body we just built inside the table that's already on the page.
-        let booksTable = document.getElementById('books_table');
-        if (booksTable) {
-            booksTable.innerHTML = tableBody;
-        }
-    })
-
-    .catch(function(error) {
-        console.log(error);
-    });
-}
-
